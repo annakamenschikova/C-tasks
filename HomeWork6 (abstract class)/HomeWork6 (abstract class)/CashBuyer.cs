ba3ebfe0bf3e37c721f1a90f5f91903549c9c367
Anna Kamenschikova <annitra.w@gmail.com>
2022-04-01 15:12:38 +0300

    First commit for Task6

---
+++ HomeWork6 (abstract class)/HomeWork6 (abstract class)/CashBuyer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork6__abstract_class_
+{
+    class CashBuyer: Buyer
+    {
+        public string Cash = "Cash";
+        public override string Pay()
+        {
+            return System.String.Format("Payment method is {0}", Cash);
+        }
+    }
+}
